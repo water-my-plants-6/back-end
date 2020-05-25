73f965810ab2035b94ef9802822f7dc73a09e1d8
Angela Williams <lambda_school_loaner_97@Lambda-School-Loaner-97s-MacBook-Pro.local>
2020-05-24 17:35:33 -0700

    debugging issue for deployment

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,49 @@
+// module.exports = {
+// 	client: "sqlite3",
+// 	useNullAsDefault: true,
+// 	connection: {
+// 		filename: "./data/auth.db3",
+// 	},
+// 	migrations: {
+// 		directory: "./data/migrations",
+// 	},
+// 	seeds: {
+// 		directory: "./data/seeds",
+// 	},
+// 	pool: {
+// 		afterCreate: (conn, done) => {
+// 			conn.run("PRAGMA foreign_keys = ON", done)
+// 		},
+// 	},
+// }
+
+require('dotenv').config();
+
 module.exports = {
-	client: "sqlite3",
-	useNullAsDefault: true,
-	connection: {
-		filename: "./data/auth.db3",
-	},
-	migrations: {
-		directory: "./data/migrations",
-	},
-	seeds: {
-		directory: "./data/seeds",
-	},
-	pool: {
-		afterCreate: (conn, done) => {
-			conn.run("PRAGMA foreign_keys = ON", done)
-		},
-	},
-}
+  development: {
+    client: 'sqlite3',
+    connection: { filename: './data/auth.db3' },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: { directory: './data/seeds' },
+  },
+  production: {
+    client: "sqlite3",
+    connection: { filename: "./data/auth.db3", },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+    migrations: { directory: "./data/migrations", },
+    seeds: { directory: "./data/seeds", },
+  }
+};
