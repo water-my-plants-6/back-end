a330cc525986ce5e03def24393d2bcc850d13340
Angela Williams <lambda_school_loaner_97@Lambda-School-Loaner-97s-MacBook-Pro.local>
2020-05-26 18:34:52 -0700

    starting to add plant table and seed data

--- data/migrations/20200524093758_users.js
+++ data/migrations/20200524093758_users.js
@@ -1,12 +1,20 @@
 exports.up = async function(knex) {
-	await knex.schema.createTable("users", (table) => {
+	return knex.schema.createTable("users", (table) => {
 		table.increments()
 		table.text("username").notNull().unique()
         table.text("password").notNull()
         table.varchar("phoneNumber").notNull()
-	})
+    })
+    
+    .createTable('ingredients', tbl => {
+        tbl.increments('id');
+        tbl.string('name').notNull()
+        tbl.integer('recipe_id').notNull()
+        .references('id').inTable('recipes')
+        tbl.float('quantity')
+    })
 }
 
 exports.down = async function(knex) {
-	await knex.schema.dropTableIfExists("users")
+	return knex.schema.dropTableIfExists("users")
 }
